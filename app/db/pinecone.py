6ef078f3bdb4d604487d0bfd6755a35f4dfe8e9d
Shubh Bansal <shubhbansal44@gmail.com>
2024-09-14 02:31:21 +0530

    removed pinecode env as it is deprecated

--- app/db/pinecone.py
+++ app/db/pinecone.py
@@ -2,7 +2,7 @@ import pinecone
 from app.core.config import settings
 
 def connect_to_pinecone():
-    pinecone.init(api_key=settings.PINECONE_API_KEY, environment=settings.PINECONE_ENVIRONMENT)
+    pinecone.init(api_key=settings.PINECONE_API_KEY)
     pinecone.Index(settings.PINECONE_INDEX_NAME)
 
 def close_pinecone_connection():
